726686ca832b6035fb5ac1e0eda707151f11b105
Federico  Rosales <frosales@baufest.com>
2025-01-23 09:56:08 -0300

    add logs and exceptions

--- clients/clients/src/main/java/com/example/clients/service/ClientService.java
+++ clients/clients/src/main/java/com/example/clients/service/ClientService.java
@@ -2,13 +2,14 @@ package com.example.clients.service;
 
 import com.example.clients.dto.ClientResponse;
 import com.example.clients.dto.ClientRequest;
+import com.example.clients.exceptions.NotFoundException;
 
 import java.util.List;
 import java.util.Map;
 
 public interface ClientService {
     void saveClient(ClientRequest client);
-    List<ClientResponse> viewClients();
+    List<ClientResponse> viewClients() throws NotFoundException;
 
-    Map<String, Double> viewKpiClient();
+    Map<String, Double> viewKpiClient() throws NotFoundException;
 }
--- clients/clients/src/main/java/com/example/clients/service/ClientServiceImpl.java
+++ clients/clients/src/main/java/com/example/clients/service/ClientServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.clients.service;
 import com.example.clients.dto.ClientResponse;
 import com.example.clients.dto.ClientRequest;
 import com.example.clients.entity.Client;
+import com.example.clients.exceptions.Exceptions;
+import com.example.clients.exceptions.NotFoundException;
 import com.example.clients.repository.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,8 +15,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Service
 public class ClientServiceImpl implements ClientService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientServiceImpl.class);
 
     private ClientRepository clientRepository;
 
@@ -21,18 +28,47 @@ public class ClientServiceImpl implements ClientService {
     }
 
     @Override
-    public void saveClient(ClientRequest client) {
-        Client clientEntity = new Client();
-        clientEntity.setName(client.getName());
-        clientEntity.setLastName(client.getLastName());
-        clientEntity.setAge(client.getAge());
-        clientEntity.setDateOfBirth(client.getDateOfBirth());
-        clientRepository.save(clientEntity);
+    public void saveClient(ClientRequest clientRequest) {
+        Client client = newClient(clientRequest);
+        try{
+            clientRepository.save(client);
+            LOGGER.info("El cliente: " + clientRequest.getName() + " " + clientRequest.getLastName() + " se ha almacenado correctamente");
+        } catch (Exception e) {
+            LOGGER.error("Error: el cliente: " + clientRequest.getName() + " " + clientRequest.getLastName() + " no puedo almacenarse");
+            throw new Exceptions("Error: el cliente: " + clientRequest.getName() + " " + clientRequest.getLastName() + " no puedo almacenarse");
+        }
     }
 
     @Override
-    public List<ClientResponse> viewClients() {
+    public List<ClientResponse> viewClients() throws NotFoundException{
+        List<Client> clients = checkClients();
+        return newClientResponseList(clients);
+    }
+
+    @Override
+    public  Map<String, Double> viewKpiClient() throws NotFoundException {
+        List<Client> clients = checkClients();
+        double average = clients.stream().mapToInt(Client::getAge).average().orElse(0.0);
+        double deviation = Math.sqrt(clients.stream()
+                .mapToDouble(c -> Math.pow(c.getAge() - average, 2))
+                .average().orElse(0.0));
+        Map<String, Double> kpi = new HashMap<>();
+        kpi.put("edad_promedio", average);
+        kpi.put("edad_desviacion", deviation);
+        LOGGER.info("El promedio y la desviacion de las edades de los clientes se calcularon correctamente");
+        return kpi;
+    }
+
+    private List<Client> checkClients() {
         List<Client> clients = clientRepository.findAll();
+        LOGGER.info("Se han encontrado " + clients.size() + " clientes");
+        if (clients.isEmpty()) {
+            throw new NotFoundException("No hay clientes registrados");
+        }
+        return clients;
+    }
+
+    private List<ClientResponse> newClientResponseList(List<Client> clients) {
         List<ClientResponse> clientResponses = new ArrayList<>();
         clients.forEach(c -> {
             ClientResponse clientResponse = new ClientResponse();
@@ -45,18 +81,12 @@ public class ClientServiceImpl implements ClientService {
         });
         return clientResponses;
     }
-
-    @Override
-    public  Map<String, Double> viewKpiClient() {
-        List<Client> clients = clientRepository.findAll();
-        double average = clients.stream().mapToInt(Client::getAge).average().orElse(0.0);
-        double deviation = Math.sqrt(clients.stream()
-                .mapToDouble(c -> Math.pow(c.getAge() - average, 2))
-                .average().orElse(0.0));
-        Map<String, Double> kpi = new HashMap<>();
-        kpi.put("edad_promedio", average);
-        kpi.put("edad_desviacion", deviation);
-        return kpi;
+    private Client newClient(ClientRequest clientRequest) {
+        Client client = new Client();
+        client.setName(clientRequest.getName());
+        client.setLastName(clientRequest.getLastName());
+        client.setAge(clientRequest.getAge());
+        client.setDateOfBirth(clientRequest.getDateOfBirth());
+        return client;
     }
-
 }
